52966c5f7b1c5b33fffb94f1d39509b019525b8e
Rodney Dennis <rdnydnns@gmail.com>
2015-05-13 09:27:59 -0500

    make api call on scroll event, not just once

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 angular.module('mailApp', ['infinite-scroll'])
   .controller('MailCtrl', function($scope, $http) {
     // get the messages
-    $http.get('https://morning-falls-3769.herokuapp.com/api/messages')
-      .success(function(response) {
-        $scope.messages = response;
-        $scope.getMoreMessages = function() {
-          var lastMessage = $scope.messages[$scope.messages.length - 1];
-          for (var i = 1; i <= 20; i++) {
-            $scope.messages.push($scope.messages[i]);
-          }
-        };
-      })
-      .error(function(response) {
-        // show message if no response
-        $scope.alert = 'Uh oh! We\'re unable to get your messages at the moment';
-      })
+    $scope.messages = [];
+
+    $scope.getMoreMessages = function() {
+      $http.get('https://morning-falls-3769.herokuapp.com/api/messages?start=' + $scope.messages.length)
+        .success(function(response) {
+          $scope.messages.concat(response);
+        })
+        .error(function(response) {
+          // show message if no response
+          $scope.alert = 'Uh oh! We\'re unable to get your messages at the moment';
+        })
+    };
+
+    $scope.getMoreMessages();
+
   });
